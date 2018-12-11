f14433e8b247ed9668ce7049e0b4a99f965311ef
Felipe Norato Lacerda <fnoratol@gmail.com>
2018-12-10 22:47:34 -0200

    Create story for AppComponent

---
+++ src/stories/app.component.stories.ts
@@ -0,0 +1,6 @@
+import { AppComponent } from './../app/app.component';
+import { storiesOf } from '@storybook/angular';
+
+storiesOf('AppComponent', module).add('defalt', () => ({
+  component: AppComponent
+}));
