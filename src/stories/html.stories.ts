9491bb6f7f8f54f07ae2872f3359e93c5c37766b
Felipe Norato <flacerda@vmware.com>
2018-12-11 16:58:34 -0200

    Improve Html Stories

--- src/stories/html.stories.ts
+++ src/stories/html.stories.ts
@@ -1,9 +1,39 @@
 import { storiesOf } from '@storybook/angular';
 
-storiesOf('Raw Html Story', module).add('default', () => ({
-  template: `
+storiesOf('Raw Html Story', module)
+  .add('default', () => ({
+    template: `
     <div style="diaplay: block;">
       <h1 style="color: red;">here is a only html + in-line css story</h1>
     </div>
   `
-}));
+  }))
+  .add('define a grid', () => ({
+    template: `
+    <h1> Grid example</h1>
+    <div class="wrapper">
+    <div class="box a">A</div>
+    <div class="box b">B</div>
+    <div class="box c">C</div>
+    <div class="box d">D</div>
+    <div class="box e">E</div>
+    <div class="box f">F</div>
+  </div>`,
+    props: {}
+  }))
+  .add('typography', () => ({
+    template: `
+    <div class="container">
+      <h1> Typography example</h1>
+      <h1>Lighter, Secondary Text</h1>
+      <p>The small element is used to create a lighter, secondary text in any heading:</p>
+      <h1>h1 heading <small>secondary text</small></h1>
+      <h2>h2 heading <small>secondary text</small></h2>
+      <h3>h3 heading <small>secondary text</small></h3>
+      <h4>h4 heading <small>secondary text</small></h4>
+      <h5>h5 heading <small>secondary text</small></h5>
+      <h6>h6 heading <small>secondary text</small></h6>
+    </div>
+    `,
+    props: {}
+  }));
