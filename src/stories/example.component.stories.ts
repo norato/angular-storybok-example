20c9f7953e10fa758d228baecd21f2ee4f091ffc
Felipe Norato <flacerda@vmware.com>
2018-12-11 16:59:32 -0200

    Install knobs addon

--- src/stories/example.component.stories.ts
+++ src/stories/example.component.stories.ts
@@ -1,19 +1,23 @@
 import { storiesOf, moduleMetadata } from '@storybook/angular';
 import { AppModule } from './../app/app.module';
+import { withKnobs, boolean } from '@storybook/addon-knobs';
 
 const metadata = moduleMetadata({
   imports: [AppModule]
 });
 
 storiesOf('Example Component', module)
+  .addDecorator(withKnobs)
   .addDecorator(metadata)
   .add('with props in template', () => ({
     template: `
+    <app-grid-overlay *ngIf="showgrid"></app-grid-overlay>
     <div class="box">
       <app-example [text]="variable"></app-example>
     </div>
     `,
     props: {
-      variable: 'amazing variable'
+      variable: 'amazing variable',
+      showgrid: boolean('Show Grid Overlay', false)
     }
   }));
