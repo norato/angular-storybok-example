b74da43c2a681c2a257631455590fff7601dfd60
Felipe Norato Lacerda <fnoratol@gmail.com>
2021-03-12 20:32:54 -0300

    Fix stories

--- src/stories/addons/links.stories.ts
+++ src/stories/addons/links.stories.ts
@@ -1,11 +1,14 @@
-import { storiesOf } from '@storybook/angular';
 import { Button } from '@storybook/angular/demo';
 import { linkTo } from '@storybook/addon-links';
 
-storiesOf('Addons | Links', module).add('default', () => ({
+export default {
+  title: 'Addons / Links',
+};
+
+export const Default = () => ({
   component: Button,
   props: {
-    text: 'Go to Welcome Story',
-    onClick: linkTo('Welcome')
+    text: 'Go to Addons / Actions',
+    onClick: linkTo('Addons / Actions')
   }
-}));
+});
